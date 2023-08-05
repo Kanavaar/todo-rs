97ba3134bf19e3a42d852242dd0b37f5836a442d
Kanavaar <uju8765@gmail.com>
2023-08-05 17:56:28 +0000

    todo::Todo: add add function and default trait

--- crates/todo-lib/src/todo.rs
+++ crates/todo-lib/src/todo.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use owo_colors::OwoColorize;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Todo {
@@ -6,7 +7,7 @@ pub struct Todo {
     modified_at: String,
     done: bool,
     title: String,
-    id: u64,
+    id: usize,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -23,6 +24,36 @@ impl Todo {
         use rand::Rng;
         let id: usize = rand::thread_rng().gen_range(1..=10000);
         id + rand::thread_rng().gen_range(1..=10000)
+    }
+
+    fn add(title: String) {
+        if title.len() < 1 {
+            println!("{error}: {msg}", error = "Error".red().bold(), msg = "No title provided, please provide a title".italic());
+
+            return;
+        }
+
+        let mut todos = crate::utils::get_todos().unwrap();
+        let todo = Todo {
+            title: String::from(&title),
+            ..Default::default()
+        };
+
+        todos.push(todo);
+        crate::utils::save_todos(todos);
+        println!("{msg}{title}", msg = "Added Todo: ".green().bold(), title = title.as_str().italic());
+    }
+}
+
+impl Default for Todo {
+    fn default() -> Todo {
+        Todo {
+        title: "".to_string(),
+        modified_at: Todo::timestamp(),
+        created_at: Todo::timestamp(),
+        done: false,
+        id: Todo::id(),
+        }
     }
 }
 
