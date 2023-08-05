927db7b26699b168de33e894383a537d488f3a36
Kanavaar <uju8765@gmail.com>
2023-08-05 14:52:13 +0000

    Project Structure with Nix and Direnv

---
+++ flake.nix
@@ -0,0 +1,83 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk.url = "github:nix-community/naersk";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs = inputs @ {
+    flake-parts,
+    fenix,
+    naersk,
+    treefmt-nix,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        treefmt-nix.flakeModule
+      ];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        rust-toolchain = fenix.packages.${system}.fromToolchainFile {
+          dir = ./.;
+          sha256 = "sha256-R0F0Risbr74xg9mEYydyebx/z0Wu6HI0/KWwrV30vZo=";
+        };
+        naersk-lib = naersk.lib.${system}.override {
+          cargo = rust-toolchain;
+          rustc = rust-toolchain;
+        };
+        nativeBuildInputs = with pkgs; [
+          rust-toolchain
+        ];
+        bin = naersk-lib.buildPackage {
+          src = ./.;
+          doCheck = true;
+          pname = "rodos";
+          version = "0.1.0";
+          inherit nativeBuildInputs;
+        };
+      in {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+        packages = {
+          default = bin;
+        };
+        devShells.default = pkgs.mkShell {
+          name = "devshell";
+          inherit nativeBuildInputs;
+          packages = [
+            config.treefmt.build.wrapper
+          ];
+        };
+
+        treefmt.config = {
+          projectRootFile = "flake.nix";
+          programs.rustfmt.enable = true;
+          programs.alejandra.enable = true;
+          settings.formatter.alejandra.options = ["-q"];
+        };
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+    };
+}
